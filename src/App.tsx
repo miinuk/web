f6e5536d48088afa4b5bba0840b7fb11399aac1e
willian <willianpereira39@hotmail.com>
2022-09-23 23:15:04 -0300

    alterações page

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,84 @@
   //any = qualquer coisa
   //para ser tratado como javascript tem que colocar {} em volta
   // ao colocar algo em uma interface aquela propriedade vira obrigatoria
-interface ButtonProps{
-  title : string;
-}
+import './styles/main.css';
+// importação de img usando JS
+//bg-clip-text serve pra imagem  ficar por baixo do texto como fundo
+import logoImg from './assets/logo.svg';
 
-function Button (props: ButtonProps){
-    return (
-        
-        <button> 
-          {props.title}
-        </button>   
-    )
-}
 
 function App(){
 
-  return <div>
-        <Button title= "send 1"/>
-        <Button title= "send 2"/>
-        <Button title= "send 3"/>
-        </div>
+  return (
+      <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20' >
+          <img src={logoImg} alt=""  />
+
+          <h1 className='text-6xl text-white font-black mt-20'>
+            Seu <span className='text-transparent bg-nlw-gradient bg-clip-text' >duo</span> está aqui.
+          </h1>
+
+          <div className='grid grid-cols-6 gap-6 mt-16' >
+
+            
+
+            <a href="" className="relative rounded-lg overflow-hidden">
+              <img src="/game-1.png" alt="" />
+            
+            <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+              <strong className='font-bold text-white block'>Fortnite</strong>
+              <span className='text-zinc-300 text-sm block '>4 anúncios</span>
+              </div>  
+            </a>
+
+            <a href=""className="relative"> 
+              <img src="/game-2.png" alt="" />
+
+              <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+              <strong className='font-bold text-white block'>League of Legends</strong>
+              <span className='text-zinc-300 text-sm block '>4 anúncios</span>
+              </div> 
+            </a>  
+
+            <a href=""className="relative">
+              <img src="/game-3.png" alt="" />
+
+              <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+              <strong className='font-bold text-white block'>Dota 2</strong>
+              <span className='text-zinc-300 text-sm block '>4 anúncios</span>
+              </div> 
+            </a>
+
+            <a href=""className="relative">
+              <img src="/game-4.png" alt="" />
+
+              <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+              <strong className='font-bold text-white block'>Counter-Strike</strong>
+              <span className='text-zinc-300 text-sm block '>4 anúncios</span>
+              </div> 
+            </a>
+
+            <a href=""className="relative">
+              <img src="/game-5.png" alt="" />
+
+              <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+              <strong className='font-bold text-white block'>Apex Legends</strong>
+              <span className='text-zinc-300 text-sm block '>4 anúncios</span>
+              </div> 
+            </a>  
+
+            <a href=""className="relative">
+              <img src="/game-6.png"alt="" />
+
+              <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+              <strong className='font-bold text-white block'>World of Warcraft</strong>
+              <span className='text-zinc-300 text-sm block '>4 anúncios</span>
+              </div> 
+            </a>   
+          </div>
+      </div>
+
+      
+  )
 }
 
 export default App
